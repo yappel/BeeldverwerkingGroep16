691146d34a05a6247ede27639d3b7b5b9b75d7c0
yappel <y.a.appel@student.tudelft.nl>
2015-01-09 15:30:40 +0100

    The segmentation now converst the image after the thresholding into a grayscale, smoothens it, and finds the edges.

--- thresholdFilter.m
+++ thresholdFilter.m
@@ -7,16 +7,18 @@ mask2 = frame(:,:,2) >= 0.48 .* frame(:,:,1) - 4.1;
 mask3 = frame(:,:,3) <= 0.68 .* frame(:,:,2) + 4.18;
 mask4 = hsidata(:,:,1) < 0.25;
 mask =  mask1 .* mask2 .* mask3 .* mask4;
-SE = strel('square',5);
-SE2 = strel('square',20);
+SE = strel('square',25);
+SE2 = strel('square',10);
+mask = imdilate(mask,SE2);
 mask = imopen(mask,SE);
-mask = imclose(mask,SE2);
-SE2= strel('square',30);
-mask = imopen(mask,SE2);
 data2(:,:,1) = uint8(mask) .* frame(:,:,1);
 data2(:,:,2) = uint8(mask) .* frame(:,:,2);
 data2(:,:,3) = uint8(mask) .* frame(:,:,3);
 
+data2 = rgb2gray(data2);
+data2 = medfilt2(data2,[3 3]);
+data2= edge(data2, 'sobel');
+%data2 = imfill(data2, 'holes');
 end
 
  
